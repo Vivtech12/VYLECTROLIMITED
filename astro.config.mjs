e2c925fb747137e779030f55ecd6d45b15e0ff0a
unknown <joeyk2k@gmail.com>
2025-11-10 19:33:35 +0900

    init

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig, fontProviders } from "astro/config";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from '@tailwindcss/vite'
+
+// https://astro.build/config
+export default defineConfig({
+	vite :{
+		plugins: [
+			tailwindcss(),
+		  ],
+	},
+	site: "https://rstradesleedsltd.com",
+	integrations: [icon(), sitemap()],
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Roboto Mono",
+				cssVariable: "--font-roboto-mono",
+				weights: [400, 500, 600, 700],
+				styles: ["normal"],
+				subsets: ["latin"],
+				fallbacks: ["monospace"]
+			},
+			{
+				provider: fontProviders.google(),
+				name: "Roboto",
+				cssVariable: "--font-roboto",
+				weights: [400, 500, 600, 700],
+				styles: ["normal"],
+				subsets: ["latin"],
+				fallbacks: ["sans-serif"]
+			}
+		]
+	}
+});																																																																													
